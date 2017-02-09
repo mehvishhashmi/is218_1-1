c05479067f4c3bcc574a5b87ceb359982aa39166
mehvishhashmi <mh449@njit.edu>
2017-02-09 12:31:06 -0500

    escape the unformatted input

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
   
   // get the data from the form
   $product_description = $_POST['product_description'];
+  
 
+
+  // escape the unformatted input
+   $product_description_escaped = htmlspecialchars($product_description);
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,7 +18,7 @@
   <main>
 	<h1> Product Discount Calculator </h1>
 	<label> Product Description:</label>
-	<span><?php echo $product_description; ?></span><br>
+	<span><?php echo $product_description_escaped; ?></span><br>
 
 
   </main>
