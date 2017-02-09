e4fd9dc6c4726f647a532a44aa1e7dfcc13b309e
mehvishhashmi <mh449@njit.edu>
2017-02-09 12:45:27 -0500

    adding discount

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
   // get the data from the form
   $product_description = $_POST['product_description'];
   $list_price = $_POST['list_price']; 
+  $discount_percent = $_POST['discount_percent'];
 
+  // calculate the discount
+  $discount = $list_price * $discount_percent * .01;
+  $discount_price = $list_price - $discount;
+  
   // apply currency formatting to the dollar amount
    $list_price_formatted = "$".number_format($list_price, 2);
  
@@ -27,6 +32,8 @@
 	<label>List Price:</label>
 	<span><?php echo $list_price_formatted; ?></span><br>
  
+ 	<label>Standard Discount:</label>
+	<span><?php echo $discount_percent; ?></span><br>
  </main>
 </body>
 
