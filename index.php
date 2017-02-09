aeda4f5e823f80c9e970ae8e50143fd3fde3c721
mehvishhashmi <mh449@njit.edu>
2017-02-09 12:00:47 -0500

    Adding heading for the calculator

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
 <html>
 <head>
         <title> Product Discount Calculator </title>
+	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
+<body>
+  <main>
+	<h1> Product Discount Calculator </h1>
+
+
+
+
+  </main>
+</body>
 
 </html>
 
